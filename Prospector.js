c14486eee9b0d4787d04a07b91f78558e15229a2
jarekb84 <jarekb@gmail.com>
2013-11-03 11:06:22 -0600

    added minimum quantity threshold setting

--- Prospector.js
+++ Prospector.js
@@ -11,11 +11,12 @@
 	     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 	};
 
-    function ItemType(elementClass, name, action, compareStats) {
+    function ItemType(elementClass, name, action, compareStats, quantityThreshold) {
         this.ElementClass = elementClass || null;
         this.Name = name || null;
-        this.Action = action || null; // sell, drink, craft, stash
-        this.CompareStats = compareStats || null; // checks to see if stats have been imporved, if not then sells
+        this.Action = action || null; // sell, drink, craft, stash        
+        this.CompareStats = compareStats || false; // checks to see if stats have been imporved, if not then sells        
+        this.QuantityThreshold = quantityThreshold || null; // Action will not be executed unless the count of items of a type exceed this number, minimum number required to execute action
     }
 
     function Item() {
@@ -39,44 +40,46 @@
 
 	var itemTypes = [	
 		// Gems
-		new ItemType('itemtype100','Tiny Ruby','sell'),
-		new ItemType('itemtype101','Small Ruby','sell'),
-		new ItemType('itemtype102','Big Ruby','sell'),
+		new ItemType('itemtype100','Tiny Ruby','sell', false, 0),
+		new ItemType('itemtype101','Small Ruby','sell', false, 0),
+		new ItemType('itemtype102','Big Ruby','sell', false, 0),
 
-		new ItemType('itemtype110','Tiny Diamond','sell'),
-		new ItemType('itemtype111','Small Diamond','sell'),
-		new ItemType('itemtype112','Big Diamond','sell'),
+		new ItemType('itemtype110','Tiny Diamond','sell', false, 0),
+		new ItemType('itemtype111','Small Diamond','sell', false, 0),
+		new ItemType('itemtype112','Big Diamond','sell', false, 0),
 
-		new ItemType('itemtype120','Tiny Amethyst','sell'),
-		new ItemType('itemtype121','Small Amethyst','sell'),
-		//new ItemType('itemtype122','Big Amethyst','sell'),		
+		new ItemType('itemtype120','Tiny Amethyst','sell', false, 0),
+		new ItemType('itemtype121','Small Amethyst','sell', false, 0),
+		new ItemType('itemtype122','Big Amethyst','sell', false, 15),		
 
-		new ItemType('itemtype130','Tiny Sapphire','sell'),
-		new ItemType('itemtype131','Small Sapphire','sell'),
-		new ItemType('itemtype132','Big Sapphire','sell'),
+		new ItemType('itemtype130','Tiny Sapphire','sell', false, 0),
+		new ItemType('itemtype131','Small Sapphire','sell', false, 0),
+		new ItemType('itemtype132','Big Sapphire','sell', false, 0),
 
-		new ItemType('itemtype140','Tiny Starstone','sell'),
-		new ItemType('itemtype141','Small Starstone','sell'),
-		new ItemType('itemtype142','Big Starstone','sell'),
+		new ItemType('itemtype140','Tiny Starstone','sell', false, 0),
+		new ItemType('itemtype141','Small Starstone','sell', false, 0),
+		new ItemType('itemtype142','Big Starstone','sell', false, 0),
 
 		// Potions
-		new ItemType('itemtype8','Instant Gold Potion','drink'),
-		new ItemType('itemtype9','Instant Diamond Potion','drink'),
-		new ItemType('itemtype10','Gold Potion','drink'),
-		new ItemType('itemtype11','Diamond Potion','drink'),
-		new ItemType('itemtype12','Magic Find Potion','drink'),
+		new ItemType('itemtype8','Instant Gold Potion','drink', false, 20),
+		new ItemType('itemtype9','Instant Diamond Potion','drink', false, 20),
+		new ItemType('itemtype10','Gold Potion','drink', false, 0),
+		new ItemType('itemtype11','Diamond Potion','drink', false, 0),
+		new ItemType('itemtype12','Magic Find Potion','drink', false, 0),
 
 		// Scrolls
-		new ItemType('itemtype5','Blue scroll','craft'),
-		new ItemType('itemtype6','Yellow scroll','craft'),
-		new ItemType('itemtype7','Legendary scroll','craft'),
+		new ItemType('itemtype5','Blue scroll','craft', false, 0),
+		new ItemType('itemtype6','Yellow scroll','craft', false, 0),
+		new ItemType('itemtype7','Legendary scroll','craft', false, 0),
+		new ItemType('itemtype13','Amnesia Scroll','sell', false, 5), // don't craft this one unless you want your stats reset automatically
+		
 
 		// Items
-		new ItemType('itemtype0','pickaxe','sell', 'true'),
-		new ItemType('itemtype1','armor','sell', 'true'),
-		new ItemType('itemtype2','helm','sell', 'true'),
-		new ItemType('itemtype3','ring','sell', 'true'),
-		new ItemType('itemtype4','amulet','sell', 'true')
+		new ItemType('itemtype0','pickaxe','sell', true, 0),
+		new ItemType('itemtype1','armor','sell', true, 0),
+		new ItemType('itemtype2','helm','sell', true, 0),
+		new ItemType('itemtype3','ring','sell', true, 0),
+		new ItemType('itemtype4','amulet','sell', true, 0)
 	]
 
 	function appendNodeListToArray(targetArray, nodeList ){
@@ -86,21 +89,23 @@
 		return targetArray;
 	}
 	
-	function generateOutput(itemType, item, areaName) {
+	function generateOutput(itemType, item, areaName, message) {
 		var output = [
 			addSuffixPadding(itemType.Action, 7),
 			addSuffixPadding(itemType.Name, 25),
 			'from ' + addSuffixPadding(areaName, 7),
-			'on ' + item.ActionExecuteTime.today() + " @ " + item.ActionExecuteTime.timeNow()
+			'on ' + addSuffixPadding(item.ActionExecuteTime.today() + " @ " + item.ActionExecuteTime.timeNow(), 26)
 		];
 
 		if(itemType.CompareStats){
-			output.push('     stats Change: ');
+			output.push(' stats Change: ');
 			output.push(' GPS:' + addSuffixPadding(parseInt(item.Stats.goldPerSec, 10), 11));
 			output.push(' DPS:' + addSuffixPadding(parseInt(item.Stats.diamondsPerSec, 10), 7));
 			output.push(' MF:' + addSuffixPadding(parseFloat(item.Stats.magicFind).toFixed(2), 7));
 			output.push(' XP:' + addSuffixPadding(parseInt(item.Stats.maxXpPerSec, 10), 6));
 		}
+
+		output.push(message);
 
 		return output.join('');
 	}
@@ -112,14 +117,20 @@
 				findsAreaList = document.getElementById('finds'),
 				areaName = 'stash',
 				itemProcessed = false,				
-				output;				
+				output,
+				message = '',
+				countItemsOfType = 0;				
 	
 			itemTypeList = appendNodeListToArray(itemTypeList, stashAreaList.getElementsByClassName(itemType.ElementClass));
 			itemTypeList = appendNodeListToArray(itemTypeList, findsAreaList.getElementsByClassName(itemType.ElementClass));
 			
+			var gemCoutner = 0;
+
 			itemTypeList.every(function(itemElement){
 				var item = new Item();					
 				
+				countItemsOfType++;
+
 				if(itemType.CompareStats){
 					item.Id = itemElement.getAttribute('stashid');
 					if(!item.Id){
@@ -131,8 +142,16 @@
 					//if(item.Stats.goldPerSec > 0 || item.Stats.diamondsPerSec > 0 || item.Stats.magicFind > 0 || item.Stats.maxXpPerSec > 0) {
 					if(item.Stats.goldPerSec > 0) {
 						// at least one of the stats is better then the currnetly equipped itemType, don't sell
-						item.ExecuteAction  = false;						
+						item.ExecuteAction  = false;
 					}
+				}
+				
+				// don't execute action unless the item count exceeds threshold				
+				if(itemType.QuantityThreshold >= countItemsOfType){
+					item.ExecuteAction  = false;
+				} else if(itemType.QuantityThreshold > 0) {
+					// only do so for threshold > 0 so that the message is not printed for every item action
+					message += ' QuantityThreshold of ' + itemType.QuantityThreshold  + ' exceeded';					
 				}
 
 				// skips to next item in itemTypeList
@@ -141,14 +160,14 @@
 				}
 
 				item.ActionElement = itemElement.getElementsByClassName(itemType.Action)[0];
-				
+
 				// make sure element exists or is found
 				if(item.ActionElement){					
 					item.ActionElement.click();				
 					item.ActionExecuteTime = new Date();				
 					itemProcessed = true;
 					
-					output = generateOutput(itemType, item, areaName);
+					output = generateOutput(itemType, item, areaName, message);
 					console.log(output);
 
 					// exists the itemTypeList loop
--- README.md
+++ README.md
@@ -9,14 +9,28 @@ The rest is taken care of by the Prospector.
 
 ## Features
 
-* Takes actions against items in the Latest Finds and Stash
 * Sell any item
 * Drink potions
 * Craft scrolls
 * For equipement, can compare stats to your currenlty equipped set, and will sell worse ones
 * Logs actions to console, so you can see what has been sold, when, and how the stats differed
+* Set a minimum number of items that are stored in stash before an action is executed. Great way to keep a set number of gems in stock, but not too many.
+* Takes actions against items in the Latest Finds and Stash
 
 Feature requests are welcome and can be submitted [here](https://github.com/jarekb84/GoldenMinerProspector/issues).
+
+## Changelog
+
+<dl>
+	<dt>v1.1.0 released 2013-11-03</dt>
+	<dd>
+		Added minimum quantity threshold before an action is executed.
+	</dd>
+	<dt>v1.0.0 released 2013-11-02</dt>
+	<dd>
+		Initial version of script. Sells, drinks, and crafts items. Can compare equippable items before selling. Logs all actions to console.
+	</dd>
+</dl>
 
 ## Instructions
 
@@ -32,15 +46,49 @@ You should start seeing console log messages like this once items are sold.
 	craft Legendary scroll        from stash on 2013/11/02 @ 14:41:17
 	sell  Small Sapphire          from stash on 2013/11/02 @ 14:42:19
 	sell  amulet                  from finds on 2013/11/02 @ 14:44:42     stats Change:  GPS:-28496467  DPS:-64564 MF:-2.36  XP:-34  
+	sell  Big Amethyst            from stash on 2013/11/03 @ 10:24:45     QuantityThreshold of 15 exceeded
+
+## Settings
+The ItemType takes the following parameters, when creating a new ItemType object to pass to the itemTypes array, be mindful of the order of these values.
+	
+	ItemType(elementClass, name, action, compareStats, quantityThreshold)
+
+<dl>
+  <dt>elementClass (string)</dt>
+  <dd>
+  	This is the html class that points to the item the Prospector will look for in the game page.
+  </dd>
+
+  <dt>name (string)</dt>
+  <dd>
+  	This name is what will be printed out in the console window when an action is executed.
+  </dd>
+
+  <dt>action (string)</dt>
+  <dd>
+  	The type of action to execute. Currently <strong>'sell'</strong>, <strong>'drink'</strong>, <strong>'craft'</strong> values are suppored.
+  </dd>
+
+  <dt>compareStats (bolean)</dt>
+  <dd>
+  	Set this to <strong>true</strong> to compare an item to what is currnetly equipped (reccomended for wearable items).
+  	
+  	Set it to <strong>false</strong> for items that don't have stats (gems, scrolls, potions).
+  </dd>
+
+  <dt>quantityThreshold (integer)</dt>
+  <dd>
+  	An action will not be executed unless the count of items of a particular type is greater then the quantityThreshold value passed in. Set this to 0 to always execute the action if an item of the type is found. Set it to any other positive value to prevent an action unless the count is greater than the threshold. Examples would be to only sell Big Amethyst if there are already 15 in the stash or only drinking Instant Gold Potions if there are already 20 in the stash. This is meant to be used as a way to keep valuable itmes, but not let them take up all the space in the stash.
+  </dd>
+</dl>
 
 ## Disabling items
 
 If you don't want the Prospector to take action on an item, just add "//" to the line to comment the item out as I did with Big Amethyst in the itemTypes array.
-
 		
-		new ItemType('itemtype120','Tiny Amethyst','sell'),
-		new ItemType('itemtype121','Small Amethyst','sell'),
-		//new ItemType('itemtype122','Big Amethyst','sell'),	
+	new ItemType('itemtype120','Tiny Amethyst','sell', false, 0),
+	new ItemType('itemtype121','Small Amethyst','sell', false, 0),
+	//new ItemType('itemtype122','Big Amethyst','sell', false, 15),	
     
 ## Adding items
 
@@ -49,18 +97,22 @@ If you want to add new items, (in chrome)
 * click inspect element
 * get the itemtype number and name
 
-In the screenshot below, you can see that Diamond potions are itemtype11, so just add this line to the itemTypes array.
+
+In the screenshot below, you can see that Diamond potions are itemtype11, so just add this line to the itemTypes array. Look at the settings section above to determine what values to include for the paramaters.
 	
-	new ItemType('itemtype11','Diamond potion','sell'),
-	
+	new ItemType('itemtype11','Diamond Potion','drink', false, 0),
+
+In this case, the Prospector will drink a Diamond Potion everytime it is found in the stash or finds area. The prospector will not attempt to compare the potion to an item already equipped.
+
 ![alt text](http://i.imgur.com/92kR9V2.png "Example of Chrome html")
+
+
 
 ## Bugs
 Right now this works for me in Chrome. Seems to be broken in FireFox and I haven't tested IE. If there is interest in those browsers, I can take a look at fixing the cross browser issues.
 Feel free to report bugs [here](https://github.com/jarekb84/GoldenMinerProspector/issues).
 
 ## Todo
-* Add a minimum quantity threshold, ie only sell Big Amethyst if there are 15 already in the stash
 * Add a socket filter, ie only sell items if they have less then 3 sockets
 * Add more filtering criteria to the item compare logic, right now only checks if gold rate is better
 * Check if enough diamonds are avaialbe before attempting to craft a scroll
